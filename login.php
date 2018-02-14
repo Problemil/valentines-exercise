7376b16ae8e4f0e1e41ec0958213e9a01e854d87
danielstena <32764564+danielstena@users.noreply.github.com>
2018-02-14 14:06:06 +0100

    Skapat functionlogin

---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+?>
+
+<form action="start.php" method="post">
+    <input type="text" name="username" value="username">
+    <input type="password" name="password" value="password">
+    <input type="submit">
+</form>
