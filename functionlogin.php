eeb2d4a2af2fdcb7f208e02c4e40988efbe20a85
danielstena <32764564+danielstena@users.noreply.github.com>
2018-02-14 16:46:01 +0100

    Login klart
    Återstår fler användare

--- functionlogin.php
+++ functionlogin.php
@@ -1,26 +1,28 @@
 <?php
+
 $users = array(
     array("daniel", 1234, "Daniel Stenå"),
     array("joakim", 1234, "Joakim Edward")
 );
-
-
 $_SESSION["users"] = $users;
 
-
+if(isset($_SESSION["loggedin"])){
+    header("location:login.php");
+    die();
+};
+ 
 if(isset($_POST["username"])){
     $_SESSION["username"] = $_POST["username"];
     $_SESSION["password"] = $_POST["password"];
 }
 
-
 for ($i = 0; $i < count($users); $i++){
     if($_SESSION["username"] == $users[$i][0] and $_SESSION["password"] == $users[$i][1]){
+        $_SESSION["fullname"] = $users[$i][2];
+        echo "<script>alert('Hello ". $_SESSION["fullname"] ."!');</script>";
         $_SESSION["loggedin"] = true; 
+       
     }
 }
-if(!isset($_SESSION["loggedin"])){
-    header("location:login.php");
-    die();
-};
+
 
