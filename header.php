484f8098863d10bc00e2fc6b429d9df5b6a03626
Joakim Edwardh <kempanedwardh@hotmail.com>
2018-02-14 14:26:13 +0100

    update

---
+++ header.php
@@ -0,0 +1,8 @@
+<html>
+
+<header class="header">
+
+    <div class="logo">Love boxes</div>
+    <div class="login"><button>utlogg</button></div>
+
+</div>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="header">
+        <?php include 'header.php'; ?>
+    </div>
+
+    <div class="content">
+        <?php include 'content.php'; ?>
+    </div>
+
+    <div class="footer">
+        <?php include 'footer.php'; ?>
+    </div>
+
+</body>
+</html>
