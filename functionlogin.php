35ca50c4f66fab3b9a4cec879b7df46d0ec236f5
danielstena <32764564+danielstena@users.noreply.github.com>
2018-02-14 14:21:32 +0100

    Slutfört inloggning

--- functionlogin.php
+++ functionlogin.php
@@ -1,1 +1,16 @@
 <?php
+if(!isset($_SESSION["loggedin"])){
+    if(isset($_POST["username"])){
+    $_SESSION["username"] = $_POST["username"];
+    $_SESSION["password"] = $_POST["password"];
+    }
+    $testUsername = "test";
+    $testPassword = "test";
+
+
+    if($_SESSION["username"] == $testUsername and $_SESSION["password"] == $testPassword ){
+        $_SESSION["loggedin"] = true; 
+        header("location:index.php");
+        die(Wooops);
+    }
+}
--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 ?>
 
-<form action="start.php" method="post">
+<form action="index.php" method="post">
     <input type="text" name="username" value="username">
     <input type="password" name="password" value="password">
     <input type="submit">
