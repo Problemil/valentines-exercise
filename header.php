ee1eb536633e182fcad2de2585104b60e43d466f
Joakim Edwardh <kempanedwardh@hotmail.com>
2018-02-14 14:30:30 +0100

    update

--- header.php
+++ header.php
@@ -3,6 +3,6 @@
 <header class="header">
 
     <div class="logo">Love boxes</div>
-    <div class="login"><button>utlogg</button></div>
+    <div class="login"><a href="clearsession.php"><button>utlogg</button></a></div>
 
 </div>
