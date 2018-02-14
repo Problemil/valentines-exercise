28ab30f8d803b631474b7c9f0939ee5ed9c6a905
Joakim Edwardh <kempanedwardh@hotmail.com>
2018-02-14 15:24:58 +0100

    update

--- header.php
+++ header.php
@@ -1,6 +1,18 @@
-<html>
+<?php include "functionlogin.php"?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Document</title>
+</head>
 
-<header class="header">
+
+<body>
+
+<div class="header">
 
     <div class="logo">Love boxes</div>
     <img src="pics/blomma.jpg" class="headPic">
