734856b8494be4cde40e1e113059dba3ba63c2f9
danielstena <32764564+danielstena@users.noreply.github.com>
2018-02-14 15:05:49 +0100

    Css till login

--- login.php
+++ login.php
@@ -1,10 +1,27 @@
 <?php
 ?>
-
-<form action="index.php" method="post">
-    <input type="text" name="username" value="username">
-    <input type="password" name="password" value="password">
-    <input type="submit">
-</form>
+<link rel="stylesheet" type="text/css" media="screen" href="style.css" />
+    
+<div id="loginDiv">
+    <form action='index.php' METHOD='POST'>      
+        <div class='nameandinput'>
+            <div class='name'>
+                Username: 
+            </div> 
+            <div class='input'>
+                <input type='text' name='username'>
+            </div>
+        </div>
+        <div class='nameandinput'>
+            <div class='name'>
+                Password: 
+            </div> 
+            <div class='input'>
+                <input type='password' name='password'>
+            </div>
+        </div>
+        <input id="submit" type="submit" value="PRESS ME">
+    </form>
+</div>
 
 
