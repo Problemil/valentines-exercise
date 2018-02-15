00881b95d5938ef24a6c82848e202aeeed1256ab
krasseman <32762094+krasseman@users.noreply.github.com>
2018-02-15 14:37:07 +0100

    Uppdatering om oss + dropdown

--- produkt.php
+++ produkt.php
@@ -7,11 +7,26 @@
     <link rel="stylesheet" type="text/css" href="style.css">
     <title>Document</title>
 <?php
+include 'header.php';
 
 ?>
+
+<style>
+  .produkth1 {
+  text-align: center;
+  }
+
+  .productTable {
+    margin: auto;
+  }
+ </style>
 </head>
 <body>
-<div class = "tabell"> 
+
+
+  <h1 class = "produkth1"> Här är en tabell på våra produkter med pris och uppskattningsnivå! </h1>
+  
+  <div class = "tabell"> 
     <?php
   $produkter = array(
     array("name" => "Chokladask", "pris" => "40kr", "details" => "4/10"),
@@ -22,12 +37,11 @@
   );
  ?>
 
- <table border="1px;">
+ <table border="1px;" class="productTable">
  <th>Produktnamn</th> <th>Pris</th> <th> Uppskattningsnivå </th>
  <?php
   foreach($produkter as $a)
   {
-
     echo "<tr><td>".$a["name"]."</td><td>".$a["pris"]."</td> <td>".$a["details"]."</td></tr>";
   }
 ?>
@@ -36,7 +50,7 @@
 
 </div>
 <?php 
-
+include 'footer.php';
 
     ?>
 
