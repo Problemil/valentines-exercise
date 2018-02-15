7f210dc4cf284f05d433fc3cb3e9b850dfd29a1d
Joakim Edwardh <kempanedwardh@hotmail.com>
2018-02-15 15:37:06 +0100

    update

--- content.php
+++ content.php
@@ -1,11 +1,12 @@
 
 <div class="content">
 <div class="startText">
+    <h1>Welcome to Love Boxes</h1>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tempor lectus eu nisl porttitor fringilla. Nunc ut ipsum ac nulla pharetra eleifend quis ut metus. Pellentesque in eleifend enim. Sed at urna tortor. Sed in semper nibh. Suspendisse convallis sem ut rutrum rhoncus. Ut gravida venenatis aliquet. Fusce sodales, purus in posuere aliquet, massa sem consequat urna, quis malesuada nisl velit a elit.
 
 Morbi ultricies euismod ipsum, nec tristique arcu viverra at. Nunc turpis augue, aliquet sed tincidunt sed, pretium et ex. Aenean quis risus orci. Sed placerat tortor nec dolor facilisis, nec lacinia libero auctor. Morbi massa diam, vulputate sollicitudin sapien sit amet, vulputate aliquam neque. Pellentesque aliquet in lorem ut egestas. Cras elementum condimentum feugiat.
 
-Phasellus elementum magna sapien, ut vestibulum ante mattis ac. Praesent faucibus, felis non porttitor maximus, velit neque rutrum justo, viverra dictum mi nunc vel nibh. Nullam sollicitudin elit ac feugiat condimentum. Quisque cursus diam sit amet est laoreet, ut viverra ante hendrerit. Nulla tempus felis eu eros posuere, eu mattis neque eleifend. Aliquam tempus congue mauris id pharetra. Ut auctor eget neque sit amet viverra. Morbi eleifend ex id est vulputate, ac semper ipsum aliquam.
-as lacinia est sed tincidunt posuere. In sed varius felis, in imperdiet libero. Donec ut eros elit. Nullam consectetur turpis id dui porttitor, in ultricies neque hendrerit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed neque urna, luctus vel aliquet ut, luctus quis nisl. Maecenas venenatis, turpis vel imperdiet iaculis, lacus lectus auctor orci, id semper augue arcu eu sapien.
+Praesent faucibus, felis non porttitor maximus, velit neque rutrum justo, viverra dictum mi nunc vel nibh. Nullam sollicitudin elit ac feugiat condimentum. Quisque cursus diam sit amet est laoreet, ut viverra ante hendrerit. Nulla tempus felis eu eros posuere, eu mattis neque eleifend. Aliquam tempus congue mauris id pharetra. Ut auctor eget neque sit amet viverra. Morbi eleifend ex id est vulputate, ac semper ipsum aliquam.
+
 <br><br><br><button>ZOOM IN</button><button>ZOOM UT</button>
 </div></div>
