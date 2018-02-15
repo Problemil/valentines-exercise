4c99c9b8a0517e27a9984f551acc9194096bba78
krasseman <32762094+krasseman@users.noreply.github.com>
2018-02-15 14:59:35 +0100

    Mini update title fix

--- produkt.php
+++ produkt.php
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Document</title>
-<?php
-include 'header.php';
 
-?>
+    <?php 
+    $pagetitle = "Produkter";
+    include 'header.php';
+   
+   
+   ?>
+ 
 
 <style>
   .produkth1 {
@@ -20,7 +16,7 @@ include 'header.php';
     margin: auto;
   }
  </style>
-</head>
+
 <body>
 
 
