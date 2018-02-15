7792d5bdd3107a441bafd8a589c758e68509fcc5
danielstena <32764564+danielstena@users.noreply.github.com>
2018-02-15 14:58:58 +0100

    Inloggning fixad(lovar)

--- clearsession.php
+++ clearsession.php
@@ -2,6 +2,11 @@
 session_start();
 
 unset($_SESSION["loggedin"]);
+if ($_SESSION["loggedin"]) {
+    
+    print_r($_SESSION["loggedin"]);
+}
+
 // session_destroy();
 header("location:login.php");
 die(wooops);
