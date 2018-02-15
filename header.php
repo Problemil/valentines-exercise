c84e08e588f2956c5e3a9b6d5c1ba67d138e3694
Joakim Edwardh <kempanedwardh@hotmail.com>
2018-02-15 15:30:09 +0100

    update

--- header.php
+++ header.php
@@ -18,6 +18,7 @@ if($_SESSION["loggedin"] == false){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
     <title>
         <?php 
         echo siteName;
--- produkter.php
+++ produkter.php
@@ -1,14 +1,19 @@
 <?php 
 include 'header.php';
-$produktDiv = '<div class="produktDiv">Hej</div>';
+include 'data.php';
+
+
+
 ?>
 
 <html>
 <div class="produktContent">
 <?php
-for($i = 0; $i < 15; $i++){
-    echo $produktDiv;
+foreach($produkter as $key => $value){
+    echo '<div class="card"><img class="card-img-top" src="' . $value["image"] . '"><div class="card-body"><h4 class="card-title">' . $value["name"] . '</h4><p class="card-text">' . $value["desc"] . '</p><p class="card-text">' . $value["price"] . '</p><a href="#" class="btn btn-primary">Buy product</a></div><div class="card-footer">Show more +</div></div>';
 }
+
+
 ?>
 </div>
 
