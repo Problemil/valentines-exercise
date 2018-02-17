db2212c80ecb7fd55b8face40c850d1a5319b27b
Joakim Edwardh <kempanedwardh@hotmail.com>
2018-02-17 13:38:53 +0100

    update

--- header.php
+++ header.php
@@ -36,18 +36,28 @@ if($_SESSION["loggedin"] == false){
 <div class="header">
 
     <div class="logo"><a href="index.php" id="logga">Love Boxes</a></div>
+    <div class="dropdown">
+    <button class="dropbtn">Favorites</button>
+    <div class="dropdown-content">
+        <a href="index.php">Start</a>
+        <a href="omoss.php">Om oss</a>    
+        <a href="produkter.php">Produkter</a>
+        <a href="recept.php">Recept</a>
+        <a href="#">Kontakta oss</a>
+    </div>
+    </div>
     <img src="pics/blomma.jpg" class="headPic">
     <img src="pics/blomma.jpg" class="headPic">
     <div class="dropdown">
     <button class="dropbtn">Menu</button>
     <div class="dropdown-content">
-    <a href="index.php">Start</a>
-    <a href="omoss.php">Om oss</a>    
-      <a href="produkter.php">Produkter</a>
-      <a href="recept.php">Recept</a>
-      <a href="#">Kontakta oss</a>
+        <a href="index.php">Start</a>
+        <a href="omoss.php">Om oss</a>    
+        <a href="produkter.php">Produkter</a>
+        <a href="recept.php">Recept</a>
+        <a href="#">Kontakta oss</a>
     </div>
-  </div>
+    </div>
   
   
     <div><a href="clearsession.php"><button>Logga ut</button></a></div>
--- produkter.php
+++ produkter.php
@@ -10,7 +10,7 @@ include 'data.php';
 <div class="produktContent">
 <?php
 foreach($produkter as $key => $value){
-    echo '<div class="card"><img class="card-img-top" src="' . $value["image"] . '"><div class="card-body"><h4 class="card-title">' . $value["name"] . '</h4><p class="card-text">' . $value["desc"] . '</p><p class="card-text">' . $value["price"] . '</p><a href="#" class="btn btn-primary">Buy product</a></div><div class="card-footer">Show more +</div></div>';
+    echo '<div class="card"><img class="card-img-top" src="' . $value["image"] . '"><div class="card-body"><h4 class="card-title">' . $value["name"] . '</h4><p class="card-text">' . $value["desc"] . '</p><p class="card-text">' . $value["price"] . '</p><a href="#" class="btn btn-primary">Buy product</a></div><button>Add as favorite</button><div class="card-footer">Show more +</div></div>';
 }
 
 
