0d7fd75c4774fa1bbdf974323cdd9808224dff92
danielstena <32764564+danielstena@users.noreply.github.com>
2018-02-17 13:46:13 +0100

    update

--- signup.php
+++ signup.php
@@ -7,7 +7,12 @@ $_SESSION["signupurllastname"] = $_SERVER['REQUEST_URI'];
 
 //här kallar vi på signupfunktionen
 signuplogin($users, $_POST["signupusername"],$_POST["signuppassword"],$_POST["signupfullName"] );
-
+echo "<h1>array</h1>";
+print_r($_SESSION["users"]);
+echo "<br/>";
+echo "<h1>array + signup</h1>";
+echo "<br/>";
+print_r($users);
 
 ?>
 
